9144197cf9346a9bdedc6200be02554783daaf96
btorrent21 <91676644+btorrent21@users.noreply.github.com>
2022-11-17 10:03:17 +0100

    Update farmacos.js

--- JS/farmacos.js
+++ JS/farmacos.js
@@ -3,6 +3,22 @@ new gridjs.Grid({
   data: [
     ['Mike', 33, 'mike@murphy.com'],
     ['John', 82, 'john@conway.com'],
+    ['Sara', 26, 'sara@keegan.com'],
+    ['Mike', 33, 'mike@murphy.com'],
+    ['John', 82, 'john@conway.com'],
+    ['Sara', 26, 'sara@keegan.com'],
+    ['Mike', 33, 'mike@murphy.com'],
+    ['John', 82, 'john@conway.com'],
+    ['Sara', 26, 'sara@keegan.com'],
+    ['Mike', 33, 'mike@murphy.com'],
+    ['John', 82, 'john@conway.com'],
+    ['Sara', 26, 'sara@keegan.com'],
+    ['Mike', 33, 'mike@murphy.com'],
+    ['John', 82, 'john@conway.com'],
+    ['Sara', 26, 'sara@keegan.com'],
+    ['Mike', 33, 'mike@murphy.com'],
+    ['John', 82, 'john@conway.com'],
     ['Sara', 26, 'sara@keegan.com']
+    
   ] 
 }).render(document.getElementById("farmacos"));
